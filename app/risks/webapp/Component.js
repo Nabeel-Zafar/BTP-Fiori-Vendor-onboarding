2b13f0013a365a0f8c7a84a935873e91abeaad40
Muhammad Subhan <muhammadsubhan221b@gmail.com>
2024-02-29 11:16:35 +0500

    clean files and basic infra

--- app/risks/webapp/Component.js
+++ app/risks/webapp/Component.js
@@ -1,11 +1,25 @@
-sap.ui.define(
-    ["sap/fe/core/AppComponent"],
-    function (Component) {
+sap.ui.define(["sap/ui/core/UIComponent", "sap/ui/core/mvc/XMLView"],
+    function (UIComponent, XMLView) {
         "use strict";
 
-        return Component.extend("ns.risks.Component", {
+        return UIComponent.extend("ns.risks.Component", {
             metadata: {
                 manifest: "json"
+            },
+
+            init: function () {
+                // call the init function of the parent
+                UIComponent.prototype.init.apply(this, arguments);
+
+                // create the views based on the url/hash
+                XMLView.create({
+                    viewName: "ns.risks.view.App" // Adjust the view name if necessary
+                }).then(function (oView) {
+                    oView.placeAt("content"); // Place the view in the DOM
+                });
+
+                // Initialize the router
+                this.getRouter().initialize();
             }
         });
     }
